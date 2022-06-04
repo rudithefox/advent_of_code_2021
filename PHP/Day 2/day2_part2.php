3bff013452bc02a06912d1d2f828850510df7533
Rudi Venter <rudolphthefox@gmail.com>
2022-06-04 20:43:55 +0200

    PHP Day 2 part 2

---
+++ PHP/Day 2/day2_part2.php
@@ -0,0 +1,28 @@
+<?php
+
+$file = file_get_contents(__DIR__ . "/input.txt");
+$events = explode("\n", $file);
+
+$depth = 0;
+$hor_pos = 0;
+$aim = 0;
+
+foreach ($events as $key => $event) {
+    $pair = explode(' ', $event);
+    switch ($pair[0]) {
+        case 'forward':
+            $hor_pos = $hor_pos + $pair[1];
+            $depth = $depth + $aim * $pair[1];
+            break;
+        case 'up':
+            $aim = $aim - $pair[1];
+            break;
+        case 'down':
+            $aim = $aim + $pair[1];
+            break;
+    }
+}
+
+$total = ($depth*$hor_pos);
+
+echo $total;
