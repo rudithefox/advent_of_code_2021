d5a845ab4f8333eb8f4f0a437d74f2f94d91f15c
Rudi Venter <rudolphthefox@gmail.com>
2022-06-16 20:14:42 +0200

    PHP Day 3 part 2 - finished

---
+++ PHP/Day 3/day3_part2.php
@@ -0,0 +1,75 @@
+<?php
+
+function getOxygen()
+{
+    $file = file_get_contents(__DIR__ . "/input.txt");
+    $binaries = explode("\n", $file);
+
+    $column_sets = [];
+    foreach ($binaries as $key => $binary) {
+        $set = str_split($binary);
+        foreach ($set as $key => $number) {
+            $column_sets[$key][] = $number;
+        }
+    }
+
+    $column = 0;
+    while (count($binaries) > 1) {
+        $freqs = array_count_values($column_sets[$column]);
+        // this needs to be changed to include the if equal dfeault stuff
+        $common = ($freqs[1] >= $freqs[0] ? 1 : 0);
+        foreach ($column_sets[$column] as $key => $value) {
+            if ($value != $common) {
+                unset($binaries[$key]);
+                foreach ($column_sets as $set_key => $column_list) {
+                    unset($column_sets[$set_key][$key]);
+                }
+            }
+        }
+        $column++;
+    }
+
+    $binary = $binaries;
+
+    return $binary;
+}
+
+function getCO2()
+{
+    $file = file_get_contents(__DIR__ . "/input.txt");
+    $binaries = explode("\n", $file);
+
+    $column_sets = [];
+    foreach ($binaries as $key => $binary) {
+        $set = str_split($binary);
+        foreach ($set as $key => $number) {
+            $column_sets[$key][] = $number;
+        }
+    }
+
+    $column = 0;
+    while (count($binaries) > 1) {
+        $freqs = array_count_values($column_sets[$column]);
+        // this needs to be changed to include the if equal dfeault stuff
+        $uncommon = ($freqs[0] <= $freqs[1] ? 0 : 1);
+
+        foreach ($column_sets[$column] as $key => $value) {
+            if ($value != $uncommon) {
+                unset($binaries[$key]);
+                foreach ($column_sets as $set_key => $column_list) {
+                    unset($column_sets[$set_key][$key]);
+                }
+            }
+        }
+        $column++;
+    }
+
+    $binary = $binaries;
+
+    return $binary;
+}
+
+$co2 = array_values(getCO2());
+$oxygen = array_values(getOxygen());
+
+echo (bindec($co2[0]) * bindec($oxygen[0]));
