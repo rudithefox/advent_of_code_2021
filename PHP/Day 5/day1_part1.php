6e47215f27f3da1aca0328598293e610eca23450
Rudi Venter <rudolphthefox@gmail.com>
2022-06-26 15:09:05 +0200

    PHP Day 5 progress

--- PHP/Day 5/day1_part1.php
+++ PHP/Day 5/day1_part1.php
@@ -1,15 +1,35 @@
 <?php
 
+// Gets the full ranges of numbers we need to consider & marks them
+function drawAndMark($set)
+{
+    $x_range = range($set[0][0], $set[1][0]);
+    $x_range = range($set[0][1], $set[1][1]);
+    echo $set[0][0];
+    echo $set[1][0];
+var_dump($x_range);
+    //$y_range = 
+    //$hits[$value] = $hits[$value] + 1;
+}
+
+// Returns points where there are $num amount of lines
+function getOccurrances($num)
+{
+    
+}
+
+// Get input & seperate by new line
 $file = file_get_contents(__DIR__ . "/input.txt");
 $lines = explode("\n", $file);
 
-$lines = array_map(function($line){
+// Assign a number to each point
+$numbers = range(1, (1000*1000));
+$map = array_chunk($numbers, 1000, false);
+
+// Go through each line, put the sets into arrays &
+foreach ($lines as $lines_key => $line) {
     $rule = explode('->', $line);
-    $x = explode(',', $rule[0]);
-    $y = explode(',', $rule[1]);
-    $x['x1', 'x2'] = [$x];
-var_dump($rule);
-
-}, $lines);
-
-
+    $start_pos = explode(',', $rule[0]);
+    $end_pos = explode(',', $rule[1]);
+    drawAndMark([$start_pos, $end_pos]);
+}
